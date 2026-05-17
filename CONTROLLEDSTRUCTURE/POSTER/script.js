3226b2ad893fcf8704cacbaf510bc797f0515bf1
SuyunCha <suuyuunchaa@gmail.com>
2026-05-17 18:41:18 +0100

    Update script.js

--- CONTROLLEDSTRUCTURE/POSTER/script.js
+++ CONTROLLEDSTRUCTURE/POSTER/script.js
@@ -1,9 +1,47 @@
 /*
 ===================================
-POSTERS PAGE
+POSTER PREVIEW
 ===================================
 */
 
-/*
-  future interaction here
-*/
+const posters =
+  document.querySelectorAll(
+    '.poster-item img'
+  );
+
+const preview =
+  document.querySelector(
+    '.poster-preview'
+  );
+
+const previewImg =
+  preview.querySelector('img');
+
+posters.forEach(poster => {
+
+  poster.addEventListener(
+    'mouseenter',
+    () => {
+
+      preview.classList.add(
+        'active'
+      );
+
+      previewImg.src =
+        poster.src;
+
+    }
+  );
+
+  poster.addEventListener(
+    'mouseleave',
+    () => {
+
+      preview.classList.remove(
+        'active'
+      );
+
+    }
+  );
+
+});
