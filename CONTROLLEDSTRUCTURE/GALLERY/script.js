91a4eedcc248ac34e2c3c5b5bae16c56036afbaf
SuyunCha <suuyuunchaa@gmail.com>
2026-05-18 23:39:18 +0100

    Update script.js

--- CONTROLLEDSTRUCTURE/GALLERY/script.js
+++ CONTROLLEDSTRUCTURE/GALLERY/script.js
@@ -1,4 +1,4 @@
-/* CLOSE → 메인으로 (인트로 스킵) */
+/* CLOSE → 메인으로 (인트로 스킵)  */
 document.getElementById('close-btn').addEventListener('click', () => {
   sessionStorage.setItem('skipIntro', 'true');
   window.location.href = '../';
