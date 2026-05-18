6e01369ed900088023d5e3d67d45bf2dc530ded1
SuyunCha <suuyuunchaa@gmail.com>
2026-05-18 23:46:32 +0100

    Update script.js

--- CONTROLLEDSTRUCTURE/POSTER/script.js
+++ CONTROLLEDSTRUCTURE/POSTER/script.js
@@ -4,7 +4,7 @@ document.getElementById('close-btn').addEventListener('click', () => {
   window.location.href = '../';
 });
 
-/* POSTER HOVER PREVIEW */
+/* HOVER PREVIEW */
 const posters = document.querySelectorAll('.poster-item img');
 const preview = document.getElementById('poster-preview');
 const previewImg = document.getElementById('preview-img');
@@ -13,10 +13,8 @@ posters.forEach(poster => {
   poster.addEventListener('mouseenter', () => {
     previewImg.src = poster.src;
     preview.classList.add('active');
-    preview.style.pointerEvents = 'auto';
   });
   poster.addEventListener('mouseleave', () => {
     preview.classList.remove('active');
-    preview.style.pointerEvents = 'none';
   });
 });
